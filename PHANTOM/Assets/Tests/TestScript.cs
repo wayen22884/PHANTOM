07bff37958cf0a2e7eaea66cbb3b70672fe9b149
wayen22884 <65549023+wayen22884@users.noreply.github.com>
2022-03-12 21:13:56 +0800

    Test UnitTest

---
+++ PHANTOM/Assets/Tests/TestScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class TestScript
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void TestScriptSimplePasses()
+    {
+        bool IsTrue = true;
+        Assert.AreEqual(true,IsTrue);
+    }
+    
+}
