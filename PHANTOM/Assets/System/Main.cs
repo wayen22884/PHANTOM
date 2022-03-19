4afd900c1905137f74b60d5b01becfd466a5d87c
wayen22884 <65549023+wayen22884@users.noreply.github.com>
2022-03-19 20:35:31 +0800

    音樂系統上傳

---
+++ PHANTOM/Assets/System/Main.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+}
---
+++ PHANTOM/Assets/System/Music/MusicSystem.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class MusicSystem : MonoBehaviour
+{
+    [FormerlySerializedAs("Current")] [InlineButton("TestPlayMusic")] [SerializeField]
+    private AudioClip CurrentBGM;
+
+    public static MusicSystem Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private AudioSource BGMSource
+    {
+        get
+        {
+            if (!bgmSource)
+            {
+                bgmSource = gameObject.AddComponent<AudioSource>();
+            }
+
+            return bgmSource;
+        }
+    }
+
+    private AudioSource bgmSource;
+
+    private Queue<AudioSourceContainer> bgvSources;
+
+    
+    
+    private void PlayMusic()
+    {
+        BGMSource.clip = CurrentBGM;
+        BGMSource.Play();
+    }
+
+    public void PlayMusic(AudioClip audioClip)
+    {
+        CurrentBGM = audioClip;
+        PlayMusic();
+    }
+
+    public void PlayBGV(AudioClip audioClip)
+    {
+        var source = GetNotUsedBGVSource();
+        source.Play(audioClip);
+    }
+
+    private AudioSourceContainer GetNotUsedBGVSource()
+    {
+        if (bgvSources.Count > 0)
+        {
+            return bgvSources.Dequeue();
+        }
+        else
+        {
+            var source = gameObject.AddComponent<AudioSource>();
+            return new AudioSourceContainer(source, bgvSources);
+        }
+    }
+
+
+    public void TestPlayMusic()
+    {
+        PlayMusic();
+    }
+    private class AudioSourceContainer
+    {
+        private AudioSource audioSource;
+
+        private Queue<AudioSourceContainer> recycleList;
+
+        private IDisposable checkRecycleDisposable;
+
+        public AudioSourceContainer(AudioSource audioSource, Queue<AudioSourceContainer> list)
+        {
+            this.audioSource = audioSource;
+            recycleList = list;
+        }
+
+        public void SetClip(AudioClip audioClip)
+        {
+            audioSource.clip = audioClip;
+        }
+
+        public void Play()
+        {
+            audioSource.Play();
+            checkRecycleDisposable = Observable.EveryUpdate().Subscribe((_) => CheckRecycle());
+        }
+
+        public void Play(AudioClip audioClip)
+        {
+            SetClip(audioClip);
+            Play();
+        }
+
+        private void CheckRecycle()
+        {
+            if (!audioSource.isPlaying)
+            {
+                checkRecycleDisposable?.Dispose();
+                recycleList.Enqueue(this);
+            }
+        }
+    }
+}
