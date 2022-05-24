255e2d04248480f4d7151530f1fb7d20825c8e23
wayen22884 <65549023+wayen22884@users.noreply.github.com>
2022-05-24 23:21:28 +0800

    切換動畫api調整

--- PHANTOM/Assets/GameCode/Character/ICharacter.cs
+++ PHANTOM/Assets/GameCode/Character/ICharacter.cs
@@ -17,7 +17,7 @@ public abstract class ICharacter : ISourcePoolObj
     public Transform AttackPoint{ get; private set; }
     public Animator Animator { get; private set; }
 
-    public Action<Transform, string, bool> DoAnimation { get; private set; }
+    public Action<Transform, string, bool> DoAnimation { get; set; }
     public bool Death { get; protected set; }
 
     protected Transform model;
