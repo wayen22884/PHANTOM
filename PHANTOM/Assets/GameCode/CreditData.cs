b5642cb4db1ea98cb00e623babe29093cadd2af7
bogay <pojay11523@gmail.com>
2022-07-19 20:53:38 +0800

    feat(credit): setup credit scene

---
+++ PHANTOM/Assets/GameCode/CreditData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Credit")]
+public class CreditData : ScriptableObject
+{
+    public string Name;
+    public string JobPosition;
+    public string Sentence;
+}
